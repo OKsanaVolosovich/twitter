1e645709e616035919d51606dcd4f3ada44416fc
OKsana Volosovich <ok_oksana@bk.ru>
2015-02-02 13:34:23 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'help', to: 'static_pages#help'
+
+  get 'about', to: 'static_pages#about'
+
+  get 'contacts', to: 'static_pages#contacts'
+
+end
